893fef68f07565c1275061d76f83e7d6ed6ac56c
Wacharaphol Ketwongsa <k.wacharaphol@gmail.com>
2023-08-03 23:06:45 +0700

    first draft

---
+++ script.js
@@ -0,0 +1,19 @@
+let sections = document.querySelectorAll('section');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 250;
+        let height = sec.offsetHeight;
+        console.log(top, offset, height);
+
+        if (top >= offset) {
+            sec.classList.add('active');
+        } else if (top <= offset - 550) {
+            sec.classList.remove('active');
+        }
+    })
+
+}
+
+
